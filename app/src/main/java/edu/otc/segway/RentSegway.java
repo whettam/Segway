7ae09db9bca36c12ee96cdcd34c4b63c0bc715ba
whettam <mjballard88@gmail.com>
2024-02-05 09:23:44 -0600

    Initial commit

---
+++ app/src/main/java/edu/otc/segway/RentSegway.java
@@ -0,0 +1,14 @@
+package edu.otc.segway;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RentSegway extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent_segway);
+    }
+}
