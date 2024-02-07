b0dc2e067c2ae29623cf15cb0798f10da6b7754b
whettam <mjballard88@gmail.com>
2024-02-07 09:18:31 -0600

    interfaces

--- app/src/main/java/edu/otc/segway/MainActivity.java
+++ app/src/main/java/edu/otc/segway/MainActivity.java
@@ -2,13 +2,33 @@ package edu.otc.segway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.btnRent);
+        //button.setOnClickListener(new MyClick());
+        button.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Toasty", Toast.LENGTH_LONG).show();
+            Intent activity = new Intent(MainActivity.this, RentSegway.class);
+            startActivity(activity);
+        });
+    }
+
+    class MyClick implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Toasty", Toast.LENGTH_LONG).show();
+        }
     }
 }
